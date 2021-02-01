4431ecaf2aedceca2d544ff855864939789b4bb7
chenjianfeng <2429523276@qq.com>
2021-02-01 08:34:48 +0800

    提交面向对象

---
+++ C Sharp教程（中级）/面向对象编程-定义类/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 面向对象编程_定义类
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //如果要使用一个类，要先引入它所在的命名空间（namespace所指向的），由于我们这里定义的Custormer类跟这里的命名空间一致，所以可以直接使用
+            Customer customer1;//声明一个对象
+            customer1 = new Customer();//初始化对象
+            customer1.name = "阿澈是子由";//我们自己定义的类声明的对象，要先初始化才能使用
+            customer1.show();
+
+            Vector3 v1;
+            v1 = new Vector3();
+            v1.SetX(1);
+            v1.SetY(1);
+            v1.SetZ(1);
+            Console.WriteLine(v1.Length());
+        }
+    }
+}
---
+++ C Sharp教程（中级）/面向对象编程-定义类/Vector3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace 面向对象编程_定义类
+{
+    public class Vector3
+    {
+        //编程习惯上一般会把所有的字段设置成private,通过set方法去访问
+        private float x, y, z;
+        //public float x, y, z;
+        //如果我们直接在方法内部访问同名变量的时候，优先访问最近的（也就是行参）
+        //我们可以通过this.表示访问的是类的字段或方法
+        public void SetX(float x)
+        {
+            this.x = x;
+        }
+        public void SetY(float y)
+        {
+            this.y = y;
+        }
+        public void SetZ(float z)
+        {
+            this.z = z;
+        }
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        } 
+    }
+}
