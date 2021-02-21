4bd942da4702bb659c3cb8a49841d8c9ac2714b7
chenjianfeng <chenjianfeng@aobi.com>
2021-02-21 18:50:38 +0800

    泛型类和泛型函数的使用

---
+++ 中级/C Sharp教程（中级）/泛型类和泛型函数/ClassA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 泛型类和泛型函数
+{
+    //泛型类
+    class ClassA<T,A>//T代表一个数据类型,A也代表一种类型，可定义多种类型,构造的时候所有泛型都需要定义
+    {
+        private T a;
+        private T b;
+        private T c;
+
+        public ClassA(T a,T b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public string GetSum()
+        {
+            return a +""+ b;//使用""把a+b转成字符串
+        }
+    }
+}
---
+++ 中级/C Sharp教程（中级）/泛型类和泛型函数/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 泛型类和泛型函数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ca = new ClassA<int,string>(12,14);//当我们构造泛型类的时候，需要指定泛型类的类型
+            Console.WriteLine(ca.GetSum());
+
+            Console.WriteLine(GetSum(12, 34));
+            Console.WriteLine(GetSum(1.2, 34));
+            Console.WriteLine(GetSum("abc", "d"));
+
+            Console.WriteLine(GetNum<int,string,bool>(12));//泛型函数只有一种泛型的时候可以系统系别，但2种或以上时需要定义类型
+
+            Console.ReadKey();
+        }
+        public static string GetSum<T>(T a, T b)//泛型函数调用的时候指定类型
+        {
+            return a + "" + b;
+        }
+        public static T GetNum<T,T1,T2>(T a)//泛型函数也可定义多种泛型
+        {
+            return a;
+        }
+    }
+}
