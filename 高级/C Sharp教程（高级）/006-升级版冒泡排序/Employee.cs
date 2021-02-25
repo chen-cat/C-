8439725bddaaa66cfc6e26e7bd273dfa5dc09096
chenjianfeng <chenjianfeng@aobi.com>
2021-02-25 19:51:27 +0800

    利用委托、冒泡排序来从小到大排序一个顾客类，顾客包含名字和薪资的信息

---
+++ 高级/C Sharp教程（高级）/006-升级版冒泡排序/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _006_升级版冒泡排序
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public int Salary { get; private set; }
+        public Employee(string name,int salary)
+        {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public static bool Compare(Employee e1,Employee e2)//比较方法
+        {
+            if (e1.Salary>e2.Salary)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override string ToString()//重写
+        {
+            return Name+" "+Salary;
+        }
+    }
+}
--- 高级/C Sharp教程（高级）/006-升级版冒泡排序/Program.cs
+++ 高级/C Sharp教程（高级）/006-升级版冒泡排序/Program.cs
@@ -35,7 +35,41 @@ namespace _006_升级版冒泡排序
                 Console.Write(sortArray[i] + " ");
             }
 
+            Employee[] e = new Employee[]
+            {
+                new Employee("abc",123),
+                new Employee("abb",113),
+                new Employee("bbc",23),
+                new Employee("rbc",13),
+                new Employee("mnc",143),
+                new Employee("gf",8),
+            };
+            CommonSort(e, Employee.Compare);
+            foreach (var item in e)
+            {
+                Console.WriteLine(item.ToString());//需要重写.ToString方法，在Employee类中
+            }
+
             Console.ReadKey();
+        }
+
+        static void CommonSort<T>(T[] sortArray,Func<T,T,bool> compareMethod)//利用泛型声明一个数组参数，一个委托参数，数组参数为Employee类型的数组，委托为系统自带的有返回值的Func，调用CommonSort方法的时候，可以把Employee中的比较方法注册进来
+        {
+            bool isCertain = true;
+            do
+            {
+                isCertain = false;
+                for (int i = 0; i < sortArray.Length - 1; i++)
+                {
+                    if (compareMethod(sortArray[i],sortArray[i+1]))//利用委托注册进来的方法做比较
+                    {
+                        T temp = sortArray[i];
+                        sortArray[i] = sortArray[i + 1];
+                        sortArray[i + 1] = temp;
+                        isCertain = true;
+                    }
+                }
+            } while (isCertain);
         }
     }
 }
