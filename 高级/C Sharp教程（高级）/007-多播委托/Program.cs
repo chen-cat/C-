e16f8595a368ad1341db6ec11ed1a6a943e7be03
chenjianfeng <2429523276@qq.com>
2021-02-25 22:32:25 +0800

    多播委托、匿名函数和lambda表达式的使用

---
+++ 高级/C Sharp教程（高级）/007-多播委托/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _007_多播委托
+{
+    class Program
+    {
+        static void Method1()
+        {
+            Console.WriteLine("这里是方法1");
+            //throw new Exception();//抛出异常，如果多播委托其中一个方法有异常，整个委托过程就会停止
+        }
+        static void Method2()
+        {
+            Console.WriteLine("这里是方法2");
+        }
+        static void Main(string[] args)
+        {
+            //多播委托只能得到调用的最后一个方法的结果，所以多播委托的返回类型一般是声明为void
+            Action a = Method1;
+            a += Method2;
+            if (a!=null)//当一个委托没有指向任何方法的时候，调用的话会出现异常
+            {
+                a();//调用方法的顺序为注册方法的先后
+            }
+
+            //取得多播委托的所有方法
+            Delegate[] de = a.GetInvocationList();//返回Delegate的数组
+            foreach (Delegate item in de)
+            {
+                item.DynamicInvoke();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 高级/C Sharp教程（高级）/匿名方法/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace 匿名方法
+{
+    class Program
+    {
+        static int Method(int a,int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            int c = 5;
+            //Func<int, int, int> plus = Method;
+            //修改成匿名方法的形式
+            //使用匿名方法的情景：1、只需要给一个委托，并不需要其他地方调用，可以减少代码量以及复杂性 2、回调参数
+            Func<int, int, int> plus = delegate (int a, int b)
+            {
+                return a + b;
+            };
+            //lambda表达式是用来代替匿名方法的
+            Func<int, int, int> plusB1 = (a,b) =>//第一种方式,参数a、b前不需要声明类型，因为Func<int, int, int>已经声明了
+            {
+                return a + b;
+            };
+
+            Func<int, int> plusB2 = a =>//第二种方式,如果只有一个参数，就不用加（）
+            {
+                return a;
+            };
+
+            Func<int, int> plusB3 = a => a;//第三种方式,当函数体的语句只有一句时，{}和return（可返回参数的委托）可以省略
+
+            Func<int, int> plusB4 = a => a+c;//第四种方式,lambda表达式可以访问lambda表达式外部的变量，这是一个很好的功能，但是如果使用不正确会很危险
+            Console.WriteLine(plusB4(5));
+            c = 10;
+            Console.WriteLine(plusB4(5));//这个方法的结果，不但受到参数的控制，还受到c变量的控制，如果c不小心被其他地方串改了，很容易出现问题
+
+            Console.ReadKey();
+        }
+    }
+}
