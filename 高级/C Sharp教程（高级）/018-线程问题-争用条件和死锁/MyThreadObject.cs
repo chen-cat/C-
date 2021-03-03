7e8520fb5dde0cf65d4da8b7f21884f8222c9147
chenjianfeng <chenjianfeng@aobi.com>
2021-03-03 17:27:19 +0800

    提交线程问题-争用条件和死锁问题

---
+++ 高级/C Sharp教程（高级）/018-线程问题-争用条件和死锁/MyThreadObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _018_线程问题_争用条件和死锁
+{
+    class MyThreadObject
+    {
+        private int state = 5;
+
+        public void ChangeState()
+        {
+            state++;
+            if (state == 5)
+            {
+                Console.WriteLine("两个以上的线程争用，导致state出现等于5的情况");
+            }
+            state = 5;
+        }
+    }
+}
---
+++ 高级/C Sharp教程（高级）/018-线程问题-争用条件和死锁/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace _018_线程问题_争用条件和死锁
+{
+    class Program
+    {
+        static void ChangeState(object o)
+        {
+            MyThreadObject mt = o as MyThreadObject;
+            while (true)
+            {
+                lock (mt)//向系统申请可以不可锁定mt对象，如果m对象没有被锁定，那么可以。如果被锁定了，那么这个语句就会被暂停直到申请成功
+                {
+                    mt.ChangeState();//上锁后，只有一个线程在执行这个方法
+                }//释放对mt的锁
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyThreadObject mt = new MyThreadObject();
+            Thread t = new Thread(ChangeState);
+            t.Start(mt);
+
+            new Thread(ChangeState).Start(mt);
+            Console.ReadKey();
+        }
+    }
+}
