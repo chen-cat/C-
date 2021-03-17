1bae7640439edb495ebadaf2387f62f402d6fe84
chenjianfeng <chenjianfeng@aobi.com>
2021-03-17 18:49:31 +0800

    文件操作（FileInfo、DirectoryInfo、File的使用）

---
+++ 高级/C Sharp教程（高级）/028-文件操作-FileInfo、DirectoryInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _028_文件操作_FileInfo_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //相对路径：就是找当前程序所在的路径
+            FileInfo fileInfo = new FileInfo("TextFile1");
+            Console.WriteLine(fileInfo.Exists);//判断文件是否存在
+
+            //绝对路径：文件完整的路径名
+            FileInfo fileInfo1 = new FileInfo(@"D:\C-\高级\C Sharp教程（高级）\027-文件操作-FileInfo\bin\Debug\netcoreapp3.1\TextFile1");
+            Console.WriteLine(fileInfo1.Exists);
+
+            FileInfo fileInfo2 = new FileInfo("s1.txt");
+            if (fileInfo2.Exists == false)
+            {
+                fileInfo2.Create();//创建
+            }
+
+            fileInfo2.CopyTo("s2.txt");//复制
+
+            //fileInfo2.Delete();//删除
+            //文件夹的方法跟文件差不多
+            DirectoryInfo directoryInfo = new DirectoryInfo("test");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 高级/C Sharp教程（高级）/029-使用FIle读写文件/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _029_使用FIle读写文件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //读取文件，把每一行文本返回一个字符串，最好组成一个字符串数据
+            string[] strArray =  File.ReadAllLines("TextFile1.txt");
+            foreach (var item in strArray)
+            {
+                //Console.WriteLine(item);
+            }
+            //读取文件，把所有内容读取返回一个字符串
+            string s = File.ReadAllText("TextFile1.txt");
+            Console.WriteLine(s);
+            //读取文件，返回字节数组
+            byte[] byteArray = File.ReadAllBytes("11.jpg");
+            foreach (var item in byteArray)
+            {
+                //Console.WriteLine(item);
+            }
+            //创建或者重写一个文件
+            File.WriteAllText("TextFile2.txt", "你好");
+            File.WriteAllBytes("22.jpg", byteArray);
+
+            Console.ReadKey();
+        }
+    }
+}
